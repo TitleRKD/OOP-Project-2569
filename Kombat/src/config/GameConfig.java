d229008ee08e79cbfdb39d1617e56aeae27a02da
TitleRKD <sirawutpa@gmail.com>
2026-02-15 00:43:39 +0700

    Config fix

---
+++ Kombat/src/config/GameConfig.java
@@ -0,0 +1,62 @@
+package config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class GameConfig {
+
+    public long spawnCost = 100;
+    public long hexPurchaseCost = 1000;
+    public long initBudget = 10000;
+    public long initHp = 100;
+    public long turnBudget = 90;
+    public long maxBudget = 23456;
+    public long interestPct = 5;
+    public long maxTurns = 69;
+    public long maxSpawns = 47;
+
+    private static GameConfig instance;
+
+    private GameConfig() {}
+
+    public static GameConfig getInstance() {
+        if (instance == null) {
+            instance = new GameConfig();
+        }
+        return instance;
+    }
+
+    public void loadConfig(String filePath) {
+        Properties prop = new Properties();
+        try (FileInputStream input = new FileInputStream(filePath)) {
+            prop.load(input);
+
+            spawnCost = Long.parseLong(prop.getProperty("spawn_cost", "100"));
+            hexPurchaseCost = Long.parseLong(prop.getProperty("hex_purchase_cost", "1000"));
+            initBudget = Long.parseLong(prop.getProperty("init_budget", "10000"));
+            initHp = Long.parseLong(prop.getProperty("init_hp", "100"));
+            turnBudget = Long.parseLong(prop.getProperty("turn_budget", "90"));
+            maxBudget = Long.parseLong(prop.getProperty("max_budget", "23456"));
+            interestPct = Long.parseLong(prop.getProperty("interest_pct", "5"));
+            maxTurns = Long.parseLong(prop.getProperty("max_turns", "69"));
+            maxSpawns = Long.parseLong(prop.getProperty("max_spawns", "47"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public Config toConfig() {
+        Config c = new Config();
+        c.spawnCost = spawnCost;
+        c.hexPurchaseCost = hexPurchaseCost;
+        c.initBudget = initBudget;
+        c.initHp = initHp;
+        c.turnBudget = turnBudget;
+        c.maxBudget = maxBudget;
+        c.interestPct = interestPct;
+        c.maxTurns = maxTurns;
+        c.maxSpawns = maxSpawns;
+        return c;
+    }
+}
--- Kombat/src/test/GameTest.java
+++ Kombat/src/test/GameTest.java
@@ -1,4 +1,23 @@
 package test;
 
+import config.Config;
+import config.GameConfig;
+import enums.GameMode;
+import game.Game;
+import player.Player;
+
 public class GameTest {
+
+    public static void main(String[] args) {
+
+        GameConfig gameConfig = GameConfig.getInstance();
+        gameConfig.loadConfig("Kombat/src/config/Config.txt");
+        Config config = gameConfig.toConfig();
+
+        Player p1 = new Player("P1", false);
+        Player p2 = new Player("P2", false);
+
+        Game game = new Game(config, p1, p2, GameMode.SOLITAIRE);
+        game.getConfig().showConfig();
+    }
 }
